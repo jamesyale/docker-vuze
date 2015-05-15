96f0a98fb49b9ad305b7664f94d7c2b4067ed69e
James Yale <jim@thebiggame.org>
2015-05-16 00:57:14 +0100

    Add entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,13 @@
 FROM java:7
 
-RUN apt-get update ; apt-get -y install vnc4server
+RUN apt-get update ; apt-get -y install x11vnc xvfb vnc4server tightvncserver libswt-gnome-gtk-3-jni ratpoison
+
+RUN useradd -m -d /vnc vnc
+
+COPY entrypoint.sh /vnc/entrypoint.sh
+
+USER vnc
+
+CMD [ "/usr/bin/x11vnc", "--help" ]
+
+ENTRYPOINT [ "/bin/bash", "/vnc/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export USER="vnc"
+
+/bin/mkdir ~/.vnc
+
+/bin/echo "Setting password $passwd"
+
+/bin/echo $passwd | /usr/bin/tightvncpasswd -f > ~/.vnc/passwd
+
+/bin/echo "xterm &" > ~/.vnc/xstartup
+/bin/echo "/vnc/vuze/vuze &" >> ~/.vnc/xstartup
+/bin/echo "/usr/bin/ratpoison &" >> ~/.vnc/xstartup
+
+/bin/chmod +x ~/.vnc/xstartup
+
+#/usr/bin/x11vnc -forever -create -passwd $vncpasswd 
+/usr/bin/tightvncserver :0 -rfbauth ~/.vnc/passwd
+
+/usr/bin/tail -f ~/.vnc/$(/bin/hostname):0.log
