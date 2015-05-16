590387c73739bd6ca8541f42aad40f56c2c41c1d
James Yale <jim@thebiggame.org>
2015-05-16 23:05:21 +0100

    Fix permissions on passwd file to prevent prompting

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,7 @@ export USER="vnc"
 /bin/echo "/usr/bin/ratpoison &" >> ~/.vnc/xstartup
 
 /bin/chmod +x ~/.vnc/xstartup
+/bin/chmod 600 ~/.vnc/passwd
 
 #/usr/bin/x11vnc -forever -create -passwd $vncpasswd 
 /usr/bin/tightvncserver :0 -rfbauth ~/.vnc/passwd
